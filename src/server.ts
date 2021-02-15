87ae513ca444d928e3dc7d386934ec6497244188
MAURO <mfconsolani@outlook.com>
2021-02-15 11:50:20 +0100

    updated files

--- src/server.ts
+++ src/server.ts
@@ -4,7 +4,7 @@ import { productRoutes } from './products';
 
 import path from 'path'; 
 
-import handlebars from 'express-handlebars'
+import handlebars from 'express-handlebars';
 
 // Global variables
 const app:Application = express();
